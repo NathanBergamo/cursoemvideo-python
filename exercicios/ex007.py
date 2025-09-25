9744c35080bf450add41ae767020f0034a1e9140
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-09-25 00:09:06 -0300

    python

---
+++ exercicios/ex007.py
@@ -0,0 +1,4 @@
+pn = float(input('Primeira nota do aluno: '))
+sn = float(input('Segunda nota do aluno: '))
+m = (pn + sn) / 2
+print('A media entre {:.1f} e {:.1f} é igual a {:.1f}'.format(pn, sn, m))
