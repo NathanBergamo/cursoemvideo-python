dacb616265541c23aa44247dd362dc0eb5d155d3
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-09-24 23:54:37 -0300

    python

--- exercicios/ex008.py
+++
@@ -1,8 +0,0 @@
-medida = float(input('Uma distancia em metros: '))
-cm = medida * 100
-mm = medida * 1000
-dm = medida * 10
-km = medida / 1000
-hm = medida / 100
-dam = medida / 10
-print('A medida de {}m corresponde a:  \n {}cm  \n {}mm \n {}dm \n {}km \n {}hm  \n {}dam '.format(medida, cm, mm, dm, km, hm, dam))
--- exercicios/ex010.py
+++
@@ -1,4 +0,0 @@
-real = float(input('Quanto voce tem na carteira ? R$'))
-dolar = real / 5.82
-euros = real / 6.07
-print('Com o valor de R${:.2f}, você pode comprar: \n ${:.2f} \n €{:.2f}'.format(real, dolar, euros))
