268fcb8af3926ccf0ca01a5d501d037c9634bb8b
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-09-29 22:37:35 -0300

    python

---
+++ aulas/aula08a.py
@@ -0,0 +1,13 @@
+from math import sqrt, floor
+n = int(input('Digite um número: '))
+raiz = sqrt(n)
+print('A raiz de {} é igual a {:.2f}'.format(n, floor(raiz)))
+
+
+
+
+
+#import math
+#n = int(input('Digite um número: '))
+#raiz = math.sqrt(n)
+#print('A raiz de {} é igual a {:.2f}'.format(n,raiz))
