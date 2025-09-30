9fa4c8f90f52edd205efacfeb0b0c59281be2239
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-09-29 23:58:21 -0300

    python

---
+++ exercicios/ex013.py
@@ -0,0 +1,3 @@
+atual = float(input('Qual o salário do funcionário ? R$'))
+novo = atual + (atual * 15 / 100)
+print('Um funcionário que ganhava R${:.2f}, com 15 % de aumento, passa a receber R${:.2f}'.format(atual, novo))
