37cdcd1e9d9fecadfd669e151d435efdacecfe49
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-01 23:48:41 -0300

    python

---
+++ exercicios/ex017.py
@@ -0,0 +1,25 @@
+import math
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+h = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(h))
+
+
+
+#import math
+#co = float(input('Comprimento do cateto oposto: '))
+#ca = float(input('Comprimento do cateto adjacente: '))
+#h = math.hypot(co,ca)
+#print('A hipotenusa vai medir {:.2f}'.format(h))
+
+
+
+#from math import pow, sqrt
+#com1 = float(input('Comprimento do cateto oposto: '))
+#com2 = float(input('Comprimento do cateto adjacente: '))
+#co = pow(com1, 2)
+#ca = pow(com2, 2)
+#soma = co + ca
+#h = sqrt(soma)
+#print('A hipotenusa vai medir: {:.2f}'.format(h))
