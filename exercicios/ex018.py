df5b4c7859edabfa76e3f6d0f82c0ce2fb07fdc3
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-02 00:11:19 -0300

    python

---
+++ exercicios/ex018.py
@@ -0,0 +1,20 @@
+from math import sin, cos, tan, radians
+ang = float(input('Digite o ângulo que você deseja: '))
+seno = sin(radians(ang))
+cosseno = cos(radians(ang))
+tangente = tan(radians(ang))
+print('O ângulo de {:.1f} tem o SENO de {:.2f}'.format(ang, seno))
+print('O ângulo de {:.1f} tem o COSSENO de {:.2f}'.format(ang, cosseno))
+print('O ângulo de {:.1f} tem o TANGENTE de {:.2f}'.format(ang, tangente))
+
+
+
+# import math
+# ang = float(input('Digite o ângulo que você deseja: '))
+# seno = math.sin(math.radians(ang))
+# cosseno = math.cos(math.radians(ang))
+# tangente = math.tan(math.radians(ang))
+# print('O ângulo de {:.1f} tem o SENO de {:.2f}'.format(ang, seno))
+# print('O ângulo de {:.1f} tem o COSSENO de {:.2f}'.format(ang, cosseno))
+# print(('O ângulo de {:.1f} tem o TANGENTE de {:.2f}'.format(ang, tangente))
+
