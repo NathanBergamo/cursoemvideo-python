31b34ebe0e6f0ad5b5c98acbcfe38dcdf89ecf5f
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-01 23:26:51 -0300

    python

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,6 +1,6 @@
-from math import floor
+from math import trunc
 v = float(input('Digite um valor: '))
-i = floor(v)
+i = trunc(v)
 print('O valor digitado foi {} e a sua porção inteira é {}'.format(v, i))
 
 
@@ -8,5 +8,8 @@ print('O valor digitado foi {} e a sua porção inteira é {}'.format(v, i))
 
 #import math
 #v = float(input('Digite um valor: '))
-#i = math.floor(v)
+#i = math.trunc(v)
 #print('O valor digitado foi {} e a sua porção inteira é {}'.format(v, i))
+
+#v = float(input('Digite um valor: '))
+#print('O valor digitado foi {} e a sua porção inteira é {}'.format(v, int(v)))
