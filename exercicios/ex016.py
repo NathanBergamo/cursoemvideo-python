144f5b2debe0b9f6b30047b18fced4987c8e3747
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-01 23:22:20 -0300

    python

---
+++ exercicios/ex016.py
@@ -0,0 +1,12 @@
+from math import floor
+v = float(input('Digite um valor: '))
+i = floor(v)
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(v, i))
+
+
+
+
+#import math
+#v = float(input('Digite um valor: '))
+#i = math.floor(v)
+#print('O valor digitado foi {} e a sua porção inteira é {}'.format(v, i))
