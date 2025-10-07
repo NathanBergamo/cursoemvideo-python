0f65c7561b27ee697ea4636f35a372843de53829
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-06 23:22:37 -0300

    python

---
+++ exercicios/ex024.py
@@ -0,0 +1,8 @@
+cid = str(input('Em que cidade você nasceu?')).strip()
+print(cid[:5].upper() == 'SANTO')
+
+
+
+'''frase = str(input('Em Qual cidade você nasceu? ')).strip()
+a = 'Santo' in frase.capitalize()
+print(a)'''
