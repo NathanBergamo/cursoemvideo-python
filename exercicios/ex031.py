dae279e2f5adb2f191534291bfdc8fac83a3f65d
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-09 23:14:39 -0300

    python

--- exercicios/ex031.py
+++ exercicios/ex031.py
@@ -1,7 +1,5 @@
 #calculando o preço de uma passagem cobrando R$ 0,50 por km para viagens de até 200km e R$0,45 para viagens mais longas
 
-
-
 dist = float(input('Qual é a distância da sua viagem: '))
 if dist > 200:
     v = dist * 0.45
