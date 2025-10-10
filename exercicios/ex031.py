556b0d665cbfab37d59396482f6fb6a013b5c8dc
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-09 23:26:10 -0300

    python

--- exercicios/ex031.py
+++ exercicios/ex031.py
@@ -1,14 +1,22 @@
 #calculando o preço de uma passagem cobrando R$ 0,50 por km para viagens de até 200km e R$0,45 para viagens mais longas
+from tkinter.ttk import Style
 
+from colorama import Style, Fore
 dist = float(input('Qual é a distância da sua viagem: '))
 if dist > 200:
     v = dist * 0.45
-    print('Você está prestes a começar uma viagem de {:.1f}km.'.format(dist))
-    print('O preço da sua passagem será de R${:.2f}'.format(v))
+    print(Fore.LIGHTCYAN_EX + '-=-' * 8)
+    print(Fore.LIGHTYELLOW_EX + 'VIAGEM ACIMA DE 200km !'.format(dist))
+    print(Fore.LIGHTCYAN_EX + '-=-' * 8)
+    print(Fore.RESET + 'Você está prestes a começar uma viagem de', Fore.LIGHTBLUE_EX + '{:.1f}km.'.format(dist))
+    print(Fore.RESET + 'O preço da sua passagem será de', Fore.LIGHTBLUE_EX + 'R${:.2f}'.format(v))
 else:
     v = dist * 0.50
-    print('Você está prestes a começar uma viagem de {:.1f}km.'.format(dist))
-    print('O preço da sua passagem será de R${:.2f}'.format(v))
+    print(Fore.LIGHTCYAN_EX + '-=-' * 8)
+    print(Fore.LIGHTYELLOW_EX + 'VIAGEM ABAIXO DE 200km !'.format(dist))
+    print(Fore.LIGHTCYAN_EX + '-=-' *8)
+    print(Fore.RESET + 'Você está prestes a começar uma viagem de', Fore.LIGHTBLUE_EX + '{:.1f}km.'.format(dist))
+    print(Fore.RESET + 'O preço da sua passagem será de', Fore.LIGHTBLUE_EX + 'R${:.2f}'.format(v))
 
     '''v = dist * 0.45 if dist > 200 else dist * 0.50'''
 
