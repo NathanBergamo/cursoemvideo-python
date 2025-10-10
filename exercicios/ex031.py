76e807de83cbca106238a96e11d289e9d4d4aafb
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-10-09 23:11:11 -0300

    python

---
+++ exercicios/ex031.py
@@ -0,0 +1,11 @@
+dist = float(input('Qual é a distância da sua viagem: '))
+if dist > 200:
+    v = dist * 0.45
+    print('Você está prestes a começar uma viagem de {:.1f}km.'.format(dist))
+    print('O preço da sua passagem será de R${:.2f}'.format(v))
+else:
+    v = dist * 0.50
+    print('Você está prestes a começar uma viagem de {:.1f}km.'.format(dist))
+    print('O preço da sua passagem será de R${:.2f}'.format(v))
+
+    '''v = dist * 0.45 if dist > 200 else dist * 0.50'''
