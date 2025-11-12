55ac76be8d67b0e2faa968125ff0bf6f743a33c1
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-11-12 00:09:22 -0300

    python

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -7,42 +7,40 @@ print('[ 0 ] PEDRA')
 print('[ 1 ] PAPEL')
 print('[ 2 ] TESOURA')
 jogador = int(input('Qual é sua jogada? '))
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('POO!!')
-print('-='*11)
-print('Computador jogou {}'.format(itens[computador]))
-print('Jogador jogou {}'.format(itens[jogador]))
-print('-='*11)
-if computador == 0: #PEDRA
-    if jogador == 0:
-        print('EMPATE')
-    elif jogador == 1:
-        print('JOGADOR VENCE')
-    elif jogador == 2:
-        print('COMPUTADOR VENCE')
-    else:
-        print('JOGADA INVÁLIDA')
-elif computador == 1: #PAPEL
-    if jogador == 0:
-        print('COMPUTADOR VENCE')
-    elif jogador == 1:
-        print('EMPATE')
-    elif jogador == 2:
-        print('JOGADOR VENCE')
-    else:
-        print('JOGADA INVÁLIDA')
-elif computador == 2: #TESOURA
-    if jogador == 0:
-        print('JOGADOR VENCE')
-    elif jogador == 1:
-        print('COMPUTADOR VENCE')
-    elif jogador == 2:
-        print('EMPATE')
-    else:
-        print('JOGADA INVÁLIDA')
+if jogador not in [0, 1, 2]:
+    print('JOGADA INVÁLIDA! Tente novamente usando 0, 1 ou 2.')
+else:
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('POO!!')
+    print('-='*11)
+    print('Computador jogou {}'.format(itens[computador]))
+    print('Jogador jogou {}'.format(itens[jogador]))
+    print('-='*11)
+    if computador == 0: #PEDRA
+        if jogador == 0:
+            print('EMPATE')
+        elif jogador == 1:
+            print('JOGADOR VENCE')
+        elif jogador == 2:
+            print('COMPUTADOR VENCE')
+    elif computador == 1: #PAPEL
+        if jogador == 0:
+            print('COMPUTADOR VENCE')
+        elif jogador == 1:
+            print('EMPATE')
+        elif jogador == 2:
+            print('JOGADOR VENCE')
+    elif computador == 2: #TESOURA
+        if jogador == 0:
+            print('JOGADOR VENCE')
+        elif jogador == 1:
+            print('COMPUTADOR VENCE')
+        elif jogador == 2:
+            print('EMPATE')
+
 
 
 
