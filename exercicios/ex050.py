5cb661adff5c47f6d633eff526f4f58926a6fb1e
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2025-12-09 21:24:18 -0300

    python

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,6 +1,10 @@
+cont = 0
 s = 0
-for c in range(1,7):
-    n = int(input('Digite o {}º valor: '.format(c)))
-    if n % 2 == 0:
-        s += n
-print('A soma dos valores pares é de {}'.format(s))
+for c in range(1, 7):
+        n = int(input('Digite o {}º valor: '.format(c)))
+        if n % 2 == 0:
+            s += n
+            cont += 1
+print('Voce informou {} número(s) pares e a soma foi: {}'.format(cont,s))
+
+
